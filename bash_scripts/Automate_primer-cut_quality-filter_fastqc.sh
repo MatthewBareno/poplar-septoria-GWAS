d2799d4a9213207f20c4b59d3a379165cb3af6a9
MatthewBareno <barenom@oregonstate.edu>
2022-07-01 18:53:17 -0700

    added

--- bash_scripts/Automate_primer-cut_quality-filter_fastqc.sh
+++ bash_scripts/Automate_primer-cut_quality-filter_fastqc.sh
@@ -6,7 +6,7 @@
 #creates files of all fastq files in a directory
 ls ../data/*.fastq > allfiles.txt
 #cuts the file list by unique identifiers
-cut -c 45-49 allfiles.txt  > files_by_code.txt
+cut -c 50-55 allfiles.txt  > files_by_code.txt
 
 #for loop for defining and finding names of files
 for k in {1..2}; 
@@ -33,12 +33,14 @@ mv ../cleaned-data/quality_filtered/*.html ../output/quality_filtered_fastqc_htm
 #removes intermediate files for storage
 rm -r intermediate*.fastq
 rm -r ../cleaned-data/quality_filtered/*fastqc.zip
-#rm -r allfiles.txt
+rm -r allfiles.txt
 rm -r files_by_code.txt
 	
 #optional, removes the primer/quality filtered fastq files
 #rm -r  ../output/quality_filtered_fastqc_htmls/*R1.fastq
 #rm -r  ../output/quality_filtered_fastqc_htmls/*R2.fastq
 
-#resets the terminal
-
+#how to edit in different context
+##change the range in the for loop to the amount of files
+## change the range of the cut function in line 9 according to the location of the identifier of the data set
+##change the directories depending on the organization of your code
